37ff457552634769704bc24155d7a1eec132ef45
rickytabe2 <rickytabe2@gmail.com>
2026-07-21 00:55:39 +0100

    added cors policy

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="Sentinel Gov Backend", version="1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["*"], # Change this to your Vercel URL later for security
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
